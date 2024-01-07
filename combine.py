9bdd3514c8b1d5fbf1931e09f66d7d93bd5c28da
KaidenMastel <root@DESKTOP-BNPHA8Q>
2024-01-06 19:31:29 -0700

    did a thing

--- combine.py
+++ combine.py
@@ -1,5 +1,7 @@
 import requests
 from math import radians, sin, cos, sqrt, atan2
+import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def search(latitude, longitude, radius, store):
@@ -88,14 +90,48 @@ def find_closest_store(json_list, lat_param, lon_param):
                     min_distance = distance
                     closest_object = json_obj
 
-    return closest_object
-    
+    return [closest_object, distance]
 
 def distance_and_price_to_score(distance, price):
-    return price + distance * 0.00005
+    return price + distance * 0.1
 
-best_of_each_store = []
-best_of_each_store.append(find_closest_store(search(latitude, longitude, radius, "Safeway"), latitude, longitude))
-best_of_each_store.append(find_closest_store(search(latitude, longitude, radius, "Walmart"), latitude, longitude))
-best_of_each_store.append(find_closest_store(search(latitude, longitude, radius, "Costco"), latitude, longitude))
-best_of_each_store.append(find_closest_store(search(latitude, longitude, radius, "Loblaws"), latitude, longitude))
+best_of_each_store = {}
+best_of_each_store["Safeway"] = find_closest_store(search(latitude, longitude, radius, "Safeway"), latitude, longitude)
+best_of_each_store["Walmart"] = find_closest_store(search(latitude, longitude, radius, "Walmart"), latitude, longitude)
+best_of_each_store["Costco"] = find_closest_store(search(latitude, longitude, radius, "Costco"), latitude, longitude)
+best_of_each_store["Loblaws"] = find_closest_store(search(latitude, longitude, radius, "Loblaws"), latitude, longitude)
+
+data_name = "data\grocery_prices.xlsx"
+data = pd.read_excel('data\grocery_prices.xlsx')
+
+#print(data)
+
+result = data[data['Food'] == 'eggs']
+
+# for column in result:
+#     #print(column)
+#     print(result[column])
+
+# print(data.iloc[0]['Costco'])
+
+# 0 = Costco, 1 = Walmart, 2 = Safeway, 3 = Loblaws
+
+# set an arbitrary minimum pruce
+min_score = float("inf") #1000.0
+stores = ["Costco", "Walmart", "Safeway", "Loblaws"]
+best_store = ""
+for store in stores:
+    if best_of_each_store[store] == None:
+        continue
+    current_price = data.iloc[0][store]
+    current_price = current_price.item()
+    print(store)
+    print(current_price)
+    current_score = distance_and_price_to_score(best_of_each_store[store][1], current_price)
+    print(best_of_each_store[store][1])
+    print(current_score)
+    if  current_price < min_score:
+        min_score = current_score
+        best_store = store
+
+print(best_store)
